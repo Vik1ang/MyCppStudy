666cdf9b059b669226d64af0e4dc8a3a4e5b4636
Vik1ang <vik1ang@outlook.com>
2022-03-02 23:50:09 +0800

    cpp study

---
+++ bilibili/using.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+namespace wzry
+{
+	int sun_wu_kong_Id = 10;
+}
+
+void test1()
+{
+	int sun_wu_kong_Id = 20;
+
+	//using wzry::sun_wu_kong_Id;
+
+	cout << wzry::sun_wu_kong_Id << endl;
+}
+
+int main(int argc, char* argv[])
+{
+	test1();
+	return EXIT_SUCCESS;
+}
