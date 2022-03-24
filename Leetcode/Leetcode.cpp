cad4ed0acffb4fe1fdc124371a512b52db402ef7
Vik1ang <vik1ang@outlook.com>
2022-03-24 09:00:37 +0800

    lc q1

--- Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+	std::vector<int> nums = { 2,7,11,15 };
+	std::vector<int> rtn = Solution().twoSum(nums, 9);
+	for (int value : rtn)
+	{
+		std::cout << value << std::endl;
+	}
 
+	return EXIT_SUCCESS;
 }
