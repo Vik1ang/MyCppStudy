701079b9572400d4f0fab01f64310575fb0d353a
Vik1ang <vik1ang@outlook.com>
2022-03-25 10:50:10 +0800

    lc q3

---
+++ Leetcode/q3.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        int left = 0;
+        int right = 0;
+        std::unordered_map<char, int> window;
+        int length = 0;
+
+        while (right < s.size())
+        {
+            char c = s.at(right);
+            window[c]++;
+
+            while (window.at(c) == 2)
+            {
+                char d = s.at(left);
+                window[d]--;
+                left++;
+            }
+
+            length = std::max(length, right - left + 1);
+            right++;
+        }
+
+        return length;
+    }
+};
