779632fa25df0b465f77fd0777d66d4dc389c298
Vik1ang <vik1ang@outlook.com>
2022-03-27 23:19:59 +0800

    lc

---
+++ Leetcode/q4.hpp
@@ -0,0 +1,58 @@
+﻿#pragma once
+#include <vector>
+
+class Solution {
+public:
+	double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2)
+	{
+		int n = nums1.size();
+		int m = nums2.size();
+		int left = (n + m + 1) / 2;
+		int right = (n + m + 2) / 2;
+		// 将偶数和奇数的情况合并，如果是奇数，会求两次同样的k
+		return (
+			getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) +
+			getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)
+			) * 0.5;
+	}
+
+	int getKth(
+		const std::vector<int>& nums1,
+		int start1,
+		int end1,
+		const std::vector<int>& nums2,
+		int start2,
+		int end2,
+		int k
+	)
+	{
+		int len1 = end1 - start1 + 1;
+		int len2 = end2 - start2 + 1;
+		//让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+		if (len1 > len2)
+		{
+			return getKth(nums2, start2, end2, nums1, start1, end1, k);
+		}
+		if (len1 == 0)
+		{
+			return nums2[start2 + k - 1];
+		}
+
+		if (k == 1)
+		{
+			return std::min(nums1[start1], nums2[start2]);
+		}
+
+		int i = start1 + std::min(len1, k / 2) - 1;
+		int j = start2 + std::min(len2, k / 2) - 1;
+
+		if (nums1[i] > nums2[j])
+		{
+			return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+		}
+		else
+		{
+			return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+		}
+	}
+};
---
+++ Leetcode/q5.hpp
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <string>
+
+class Solution {
+public:
+	std::string longestPalindrome(std::string s) {
+		std::string res;
+		for (int i = 0; i < s.size(); ++i)
+		{
+			// 以s[i]为中心的最长回文字串
+			std::string s1 = palindrome(s, i, i);
+			std::string s2 = palindrome(s, i, i + 1);
+
+			res = res.size() > s1.size() ? res : s1;
+			res = res.size() > s2.size() ? res : s2;
+		}
+
+		return res;
+	}
+
+	std::string palindrome(const std::string& s, int l, int r)
+	{
+		// 防止索引越界
+		while (l >= 0 && r < s.size() && s[l] == s[r])
+		{
+			l--;
+			r++;
+		}
+
+		// 返回以s[l]和s[r]为中心的最长回文字串
+		return s.substr(l + 1, l > 0 ? r - l : r - l - 1);
+	}
+};
