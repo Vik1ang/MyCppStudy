1b24b7e156149939d9e54cb48cdfeab681a8ee30
Vik1ang <vik1ang@outlook.com>
2022-03-29 00:14:13 +0800

    lc

---
+++ Leetcode/q7.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <climits>
+
+class Solution {
+public:
+	int reverse(int x)
+	{
+		int rev = 0;
+		while (x != 0)
+		{
+			if (rev < INT_MIN / 10 || rev > INT_MAX / 10)
+			{
+				return 0;
+			}
+			const int digit = x % 10;
+			x /= 10;
+			rev = rev * 10 + digit;
+		}
+		return rev;
+	}
+};
