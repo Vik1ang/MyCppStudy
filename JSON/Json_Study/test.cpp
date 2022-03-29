f0caaa3eb0ac27247465f695cd8fd36f7d79c930
Vik1ang <vik1ang@outlook.com>
2022-03-29 14:54:57 +0800

    json

--- JSON/Json_Study/test.cpp
+++ JSON/Json_Study/test.cpp
@@ -269,7 +269,7 @@ int main(int argc, char* argv[])
 {
 #ifdef _WIN64
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	_CrtSetBreakAlloc(109);
+	_CrtSetBreakAlloc(88);
 #endif
 
 	test_parse();
