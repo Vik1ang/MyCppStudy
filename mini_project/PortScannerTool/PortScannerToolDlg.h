403cf12aa7cf9f87e6da29c0241877d93581b9d1
Vik1ang <vik1ang@outlook.com>
2022-04-06 09:33:29 +0800

    cpp

---
+++ mini_project/PortScannerTool/PortScannerToolDlg.h
@@ -0,0 +1,45 @@
+﻿
+// PortScannerToolDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CPortScannerToolDlg 对话框
+class CPortScannerToolDlg : public CDialogEx
+{
+// 构造
+public:
+	CPortScannerToolDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PORTSCANNERTOOL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedRadio1();
+	CIPAddressCtrl m_IP;
+	CStatic m_promot;
+	CProgressCtrl m_progress;
+	CListCtrl m_IPList;
+	CEdit m_SinglePort;
+	CEdit m_PortFrom;
+	CEdit m_Port_To;
+	CEdit m_Thread_Num;
+	afx_msg void OnBnClickedButtonStart();
+};
---
+++ mini_project/PortScannerTool/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 PortScannerTool.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PORTSCANNERTOOL_DIALOG      102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_IPADDRESS_IP                1000
+#define IDC_RADIO_SINGLEPORT            1001
+#define IDC_EDIT_SINGLE_PORT            1002
+#define IDC_RADIO_MULTIPORT             1003
+#define IDC_EDIT_PORT_TO                1004
+#define IDC_EDIT_PORT_FROM              1005
+#define IDC_EDIT_THREAD_NUMBER          1006
+#define IDC_BUTTON_START                1007
+#define IDC_BUTTON_EXIT                 1008
+#define IDC_BUTTON_STOP                 1009
+#define IDC_PROGRESS_SCAN               1010
+#define IDC_LIST_INFO                   1011
+#define IDC_STATIC_PROMPT               1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
