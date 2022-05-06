c5a127c31326ae45aed92c2a79df0d0b4764408b
Vik1ang <vik1ang@outlook.com>
2022-05-06 12:44:58 +0800

    q3

--- Leetcode/src/lc.h
+++ Leetcode/src/lc.h
@@ -9,4 +9,8 @@
 #include "q2/q2.cpp"
 #endif
 
+#if 1
+#include "q3/q3.cpp"
+#endif
+
 #endif //LEETCODE_LC_H
---
+++ Leetcode/src/q3/q3.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+namespace q3 {
+    class Solution {
+    public:
+        int lengthOfLongestSubstring(std::string s) {
+            int left = 0;
+            int right = 0;
+            std::unordered_map<char, int> window;
+            int length = 0;
+
+            while (right < s.size()) {
+                char c = s.at(right);
+                window[c]++;
+
+                while (window.at(c) == 2) {
+                    char d = s.at(left);
+                    window[d]--;
+                    left++;
+                }
+
+                length = std::max(length, right - left + 1);
+                right++;
+            }
+
+            return length;
+        }
+    };
+}
