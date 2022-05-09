c69d14c2d3cf5d7d98e4086f2f25f7cb9a3dc2b6
Vik1ang <vik1ang@outlook.com>
2022-05-09 12:07:15 +0800

    commit

--- Leetcode/src/lc.h
+++ Leetcode/src/lc.h
@@ -2,15 +2,15 @@
 #define LEETCODE_LC_H
 
 #if 1
-#include "q1/q1.cpp"
+#include "q1.cpp"
 #endif
 
 #if 1
-#include "q2/q2.cpp"
+#include "q2.cpp"
 #endif
 
 #if 1
-#include "q3/q3.cpp"
+#include "q3.cpp"
 #endif
 
 #endif //LEETCODE_LC_H
