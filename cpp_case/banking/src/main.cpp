84b53dab9c72c3a49a274b2c54588026f3f541d5
Vik1ang <vik1ang@outlook.com>
2022-07-30 21:14:22 +0800

    leetcode include初始化

--- cpp_case/banking/src/main.cpp
+++ cpp_case/banking/src/main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include "admin.h"
+#include "customer.h"
+
+void boot() {
+
+}
 
 int main() {
-    std::cout << "Hello, world" << std::endl;
+	void boot();
+
+	int condition = 0;
+
+	while (condition != 4)
+	{
+		std::cout << "You want to login as:\t\t\t\t\t\t" << std::endl << std::endl;
+		std::cout << "1-Admin" << std::endl;
+		std::cout << "2-Staff" << std::endl;
+		std::cout << "3-Customer" << std::endl;
+		std::cout << "4-Quit" << std::endl;
+
+		// get user input
+		std::cin >> condition;
+
+		if (condition == 1) {
+			admin();
+		}
+		else if (condition == 2) {
+			// TODO: ûд
+		}
+		else if (condition == 3) {
+			customer();
+		}
+		else if (condition == 4) {
+			std::cout << "Quit!!!" << std::endl;
+		}
+		else {
+			std::cout << "Wrong commands" << std::endl;
+		}
+
+	}
+
+	return EXIT_SUCCESS;
+
 }
--- cpp_case/banking/xmake.lua
+++ cpp_case/banking/xmake.lua
@@ -1,3 +1,4 @@
 target("banking")
     set_kind("binary")
     add_files("src/*.cpp")
+    add_includedirs("include")
