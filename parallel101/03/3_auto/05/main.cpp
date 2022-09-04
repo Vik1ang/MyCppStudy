ea069cda01fe4f8572dc4c769dd54e303397d583
Vik1ang <vik1ang@outlook.com>
2022-09-04 10:19:56 +0800

    parallel

---
+++ parallel101/03/3_auto/05/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <stdio.h>
+
+void test(int &) {
+    printf("int &\n");
+}
+
+void test(int const &) {
+    printf("int const &\n");
+}
+
+void test(int  &&) {
+    printf("int &&\n");
+}
+
+int main() {
+    int a = 0;
+    int *p = &a;
+    test(a);        // int &
+    test(*p);       // int &
+    test(p[a]);     // int &
+    test(1);        // int &&
+    test(a + 1);    // int &&
+    test(*p + 1);   // int &&
+
+    const int b = 3;
+    test(b);        // int const &
+    test(b + 1);    // int &&
+
+    return 0;
+}
